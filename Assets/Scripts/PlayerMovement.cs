1483fdf84d04f121ac38897592fdfadb3eebf435
khalid <sawmaster22@gmail.com>
2020-10-08 23:33:36 +0700

    fix movement and add run

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,58 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public Rigidbody2D rb;
-    Vector2 movement;
-    Vector2 change;
-    Animator animator;
+    public float runningSpeed = 8f;
+    private Rigidbody2D myRigidbody;
+    private Vector2  movement;
+    private Animator animator;
+
+    private float timeDownX = 0.0f; //time which horizontal was pressed
+    private float timeDownY = 0.0f; //time which vertical was pressed
 
     void Start()
     {
+        myRigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        change = movement;
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        if(change != movement)
+        movement = Vector2.zero;
+        float xVel = Input.GetAxisRaw("Horizontal");
+        float yVel = Input.GetAxisRaw("Vertical");
+
+        //if key is pressed on Horizontal save time it was pressed
+        if (xVel != 0)
+        {
+            if (timeDownX == 0.0f) //if we don't have a stored time for it
+                timeDownX = Time.time;
+        }
+        else
+        {
+            timeDownX = 0.0f; // reset time if no button is being pressed
+        }
+
+        //if key is pressed on vertical save time it was pressed
+        if (yVel != 0)
+        {
+            if (timeDownY == 0.0f) //if we don't have a stored time for it
+                timeDownY = Time.time;
+        }
+        else
+        {
+            timeDownY = 0.0f; // reset time if no button is being pressed
+        }
+
+        if (timeDownX > timeDownY)
+        {
+            movement = Vector2.right * xVel;
+        }
+        else if (timeDownX < timeDownY)
+        {
+            movement = Vector2.up * yVel;
+        }
+
+        if (movement != Vector2.zero)
         {
             animator.SetFloat("moveX", movement.x);
             animator.SetFloat("moveY", movement.y);
@@ -29,11 +65,22 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             animator.SetBool("moving", false);
+            animator.SetBool("running", false);
         }
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);    
+        bool isRunning = Input.GetKey(KeyCode.Z);
+        if (isRunning)
+        {
+            myRigidbody.MovePosition(myRigidbody.position + movement * runningSpeed * Time.fixedDeltaTime);
+            animator.SetBool("running", true);
+        }
+        else
+        {
+            myRigidbody.MovePosition(myRigidbody.position + movement * moveSpeed * Time.fixedDeltaTime);
+            animator.SetBool("running", false);
+        }
     }
 }
